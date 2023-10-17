35812f086a2b7d1027b14dd32e9628cd6416c672
Lucas Benchinol <lucasbenchinol@gmail.com>
2023-10-17 11:37:56 -0300

    Pequeño fix en realtimeproducts.css

--- Ejercicio-04/src/routers/realTimeProducts.router.js
+++ Ejercicio-04/src/routers/realTimeProducts.router.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/realtimeproducts', (req, res) => {
-    res.render('realtimeproducts');
+    res.render('realtimeproducts',{style: 'realtimeproducts.css'});
 });
 
 export default router;
