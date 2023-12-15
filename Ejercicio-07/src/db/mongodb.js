9fe6bdd78c88ca42959cf889076f05e889826a3f
Lucas Benchinol <lucasbenchinol@gmail.com>
2023-12-14 23:37:31 -0300

    Finalizado Ejercicio 07

--- Ejercicio-07/src/db/mongodb.js
+++ Ejercicio-07/src/db/mongodb.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-//export const URI = process.env.MONGODB_URI;
 export const URI = "mongodb+srv://developer:ZR84Vj4ZoQxi7sXZ@cluster0.fpmqsp0.mongodb.net/ecommerce";
 
 export const init = async () => {
